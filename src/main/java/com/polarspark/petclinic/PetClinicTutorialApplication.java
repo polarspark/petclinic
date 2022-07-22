b4e3adce92478a7531a2d2ef818338c44d2b5a1a
Paul Harding <paul.harding@polarspark.com>
2022-07-21 23:29:59 -0600

    Initial Commit.

---
+++ src/main/java/com/polarspark/petclinic/PetClinicTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.polarspark.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polarspark/petclinic/PetClinicTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polarspark.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
