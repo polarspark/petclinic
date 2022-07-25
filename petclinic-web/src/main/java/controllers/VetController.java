2ff56b12d666e76e8ba242fe12d677ef7b5eb2dc
Paul Harding <paul.harding@polarspark.com>
2022-07-24 23:23:19 -0600

    Adding Index Page and controllers closes #7.

--- petclinic-web/src/main/java/controllers/VetController.java
+++ petclinic-web/src/main/java/controllers/VetController.java
@@ -5,10 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class IndexController {
+public class VetController {
 
-    @RequestMapping(value = {"", "/", "index.html"}, method = RequestMethod.GET)
-    public String index() {
-        return "index";
+    @RequestMapping(value = {"/vets", "/vets/index", "/vets/index.html"}, method = RequestMethod.GET)
+
+    public String listVets(){
+        return "/vets/index";
     }
 }
