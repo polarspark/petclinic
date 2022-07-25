6342bdea5079f12681a0352f9a645803b872c8bb
Paul Harding <paul.harding@polarspark.com>
2022-07-24 18:19:24 -0600

    Impl Map Based Services, closes #6.

---
+++ petclinic-data/src/main/java/com/polarspark/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,36 @@
+package com.polarspark.petclinic.services.map;
+
+import com.polarspark.petclinic.model.Pet;
+import com.polarspark.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+}
+
