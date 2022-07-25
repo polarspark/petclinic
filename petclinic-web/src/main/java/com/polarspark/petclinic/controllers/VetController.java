5d3fe44900f0644eacc361d352a80348705765f0
Paul Harding <paul.harding@polarspark.com>
2022-07-25 01:57:07 -0600

    Added List of Vets on index page. Closes #15.

--- petclinic-web/src/main/java/com/polarspark/petclinic/controllers/VetController.java
+++ petclinic-web/src/main/java/com/polarspark/petclinic/controllers/VetController.java
@@ -1,15 +1,23 @@
 package com.polarspark.petclinic.controllers;
 
+import com.polarspark.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"","/", "/index", "/index.html"})
-    public String listVets()
-    {
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
