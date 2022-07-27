79112c32bd039cea2727905c5e3202ec193f117a
Paul Harding <paul.harding@polarspark.com>
2022-07-27 12:45:44 -0600

    Update BaseEntity to support JPA. Closes #35

--- petclinic-data/src/main/java/com/polarspark/petclinic/model/BaseEntity.java
+++ petclinic-data/src/main/java/com/polarspark/petclinic/model/BaseEntity.java
@@ -1,8 +1,16 @@
 package com.polarspark.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = javax.persistence.GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
