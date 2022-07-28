5e01b3416da22ee66e47f777159d7f7aaa8cbee1
Paul Harding <paul.harding@polarspark.com>
2022-07-27 23:41:01 -0600

    Implemented visit VisitMapService and VisitService. Closes #46

---
+++ petclinic-data/src/main/java/com/polarspark/petclinic/services/VisitService.java
@@ -0,0 +1,13 @@
+package com.polarspark.petclinic.services;
+
+import com.polarspark.petclinic.model.Visit;
+
+/**
+ * Description:
+ *
+ * @author paulh
+ * @since
+ **/
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
