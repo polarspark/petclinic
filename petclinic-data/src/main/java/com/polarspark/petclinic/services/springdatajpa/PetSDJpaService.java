bd3d39bc353e97f1dff4799268da7e3c3d1fa21a
Paul Harding <paul.harding@polarspark.com>
2022-07-27 23:16:46 -0600

    Added spring data jpa service for pets. Closes #42 Closes #43

---
+++ petclinic-data/src/main/java/com/polarspark/petclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,54 @@
+package com.polarspark.petclinic.services.springdatajpa;
+
+import com.polarspark.petclinic.model.Pet;
+import com.polarspark.petclinic.repositories.PetRepository;
+import com.polarspark.petclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Description:
+ *
+ * @author paulh
+ * @since
+ **/
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return petRepository.save(pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        petRepository.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
