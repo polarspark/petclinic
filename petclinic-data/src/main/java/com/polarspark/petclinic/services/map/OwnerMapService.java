d67d4896c5ae849ce5af78c7911b2c2ac3cab715
Paul Harding <paul.harding@polarspark.com>
2022-07-27 23:23:14 -0600

    Renamed to meet a convient in naming services.

--- petclinic-data/src/main/java/com/polarspark/petclinic/services/map/OwnerMapService.java
+++ petclinic-data/src/main/java/com/polarspark/petclinic/services/map/OwnerMapService.java
@@ -5,19 +5,18 @@ import com.polarspark.petclinic.model.Pet;
 import com.polarspark.petclinic.services.OwnerService;
 import com.polarspark.petclinic.services.PetService;
 import com.polarspark.petclinic.services.PetTypeService;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
 
     private final PetTypeService petTypeService;
     private final PetService petService;
 
-    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+    public OwnerMapService(PetTypeService petTypeService, PetService petService) {
         this.petTypeService = petTypeService;
         this.petService = petService;
     }
